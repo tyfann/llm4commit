441a23eb002fcbd76cd3150c383c122adea03f52
tyfann <1342344773@qq.com>
2024-07-01 00:25:31 +0200

    baseline and scenario data add

--- models/bloom.py
+++
@@ -1,9 +0,0 @@
-# Load model directly
-from transformers import AutoTokenizer, AutoModelForCausalLM
-
-tokenizer = AutoTokenizer.from_pretrained("bigscience/bloom-3b", cache_dir='./')
-model = AutoModelForCausalLM.from_pretrained("bigscience/bloom-3b", cache_dir='./')
-
-inputs = tokenizer("Hello, I am", return_tensors="pt")
-tokens = model.generate(**input)
-print(tokenizer.decode(tokens[0]))
